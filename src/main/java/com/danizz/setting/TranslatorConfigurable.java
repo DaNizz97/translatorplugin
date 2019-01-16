0d795f78911339337c38799a7f6f1df9555e984a
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-01-16 20:21:26 +0300

    Create a TranslatorConfigurable & add extension into plugin.xml

---
+++ src/main/java/com/danizz/setting/TranslatorConfigurable.java
@@ -0,0 +1,41 @@
+package com.danizz.setting;
+
+import com.danizz.action.TranslatorProvider;
+import com.danizz.gui.TranslationConfigure;
+import com.intellij.openapi.options.Configurable;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class TranslatorConfigurable implements Configurable {
+    private TranslationConfigure settingsGui;
+    private TranslatorProvider provider;
+
+    public TranslatorConfigurable() {
+        provider = TranslatorProvider.getInstance();
+    }
+
+    @Nls(capitalization = Nls.Capitalization.Title)
+    @Override
+    public String getDisplayName() {
+        return "Enable/Disable Translation by Double Click";
+    }
+
+    @Nullable
+    @Override
+    public JComponent createComponent() {
+        settingsGui = new TranslationConfigure();
+        return settingsGui.getRoot();
+    }
+
+    @Override
+    public boolean isModified() {
+        return settingsGui.isModified();
+    }
+
+    @Override
+    public void apply() {
+        settingsGui.selectTranslator();
+    }
+}
