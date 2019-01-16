c803f055156662b672cbc6d46bdab33618d93142
Daniil Nizovkin <dan_nizovkin_kungur@mail.ru>
2019-01-16 19:49:07 +0300

    Implement basic functionality of action

--- src/main/java/com/danizz/action/TranslateAction.java
+++ src/main/java/com/danizz/action/TranslateAction.java
@@ -1,13 +1,31 @@
 package com.danizz.action;
 
+import com.danizz.component.PopupMessageViewer;
+import com.danizz.component.TextSelector;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.ui.Messages;
+
+import java.io.IOException;
 
 public class TranslateAction extends AnAction implements DumbAware {
+
+    private PopupMessageViewer messageViewer;
+    private TextSelector textSelector;
+    private TranslatorProvider provider = TranslatorProvider.getInstance();
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Messages.showMessageDialog("First Action Example", "Example", Messages.getInformationIcon());
+        messageViewer = new PopupMessageViewer(e);
+        textSelector = new TextSelector(e);
+        String translatedMessage;
+        try {
+            translatedMessage = provider.getTranslator().translate(textSelector.getSelectedText());
+        } catch (IOException e1) {
+            translatedMessage = textSelector.getSelectedText();
+            e1.printStackTrace();
+        }
+        messageViewer.showTranslatedText(translatedMessage);
     }
+
 }
